ceab911b1a80f3c82ac4b4855926211d35352653
JTS-Dawnstar <87510267+JTS-Dawnstar@users.noreply.github.com>
2025-05-08 00:03:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def getd_temp(date, station = 888, dynamic = True):
     
     fissue = False
     
-    soup = BeautifulSoup(GET(url).read().decode(), 'html.parser')
+    soup = BeautifulSoup(GET(url).read(), 'html.parser')
     table = soup.find('tbody')
     try: 
         row = table.findChildren('tr')[date.day - 1]
@@ -157,8 +157,8 @@ def geth_temp(date, station = 888, dynamic = True, feature = 0): # date is datet
     
     fissue = False
     
-    soup = BeautifulSoup(GET(url).read().decode(), 'html.parser')
-    print(GET(url).read().decode())
+    soup = BeautifulSoup(GET(url).read(), 'html.parser')
+    print(GET(url).read())
     table = soup.find('tbody')
     try: 
         row = table.findChildren('tr')[date.hour]
