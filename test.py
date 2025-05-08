64cafa0cebaf14ca74fe6759e7d7200767126777
JTS-Dawnstar <87510267+JTS-Dawnstar@users.noreply.github.com>
2025-05-07 23:11:24 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-from pyscript import fetch
+from pyscript import fetch, display
+
+print("this is a thing")
+display("this thing is being displayed")
 
 data = await fetch("https://corsproxy.io/?url=https://example.com").text()
 
 print(data)
+display(data)
